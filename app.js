a9dd822e7d48f00e9adab9b548e7e1aa5f0527b8
Tejas <tejas.c.srikanth.2020@outlook.com>
2020-06-17 09:00:30 -0400

    use md5 for hashing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const md5 = require("md5")
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs")
@@ -16,9 +17,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
 const User = mongoose.model("User", userSchema);
 
 app.listen(3000, function(){
@@ -35,7 +33,7 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: email}, function(err, foundEmail){
     if (err){
@@ -59,7 +57,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
