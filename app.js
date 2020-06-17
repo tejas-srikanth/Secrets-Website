ff7e5568b2b6c60f5f30dd5894f04d3a1cbc4abc
Tejas <tejas.c.srikanth.2020@outlook.com>
2020-06-17 08:38:48 -0400

    add gitignore

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "dipitlowpickitupslowrollitallaroundpopitoutlikeyourbackbroke.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
